b85e215f7d9483fb84f394ba2f6dde1b5643e2a4
Sam Thomson <samt@samt.st>
2018-11-16 18:31:58 +0630

    force db seeding, no command prompt

--- www-workspace/seed.sh
+++ www-workspace/seed.sh
@@ -5,6 +5,6 @@
 cd /var/www/user-api-laravel && composer install
 cd /var/www/auto && composer install
 cd /var/www && ./migrations.sh
-cd /var/www/auto && php artisan db:seed --class=FolderSeeder
+cd /var/www/auto && php artisan db:seed --class=FolderSeeder --force
 cd /var/www/auto && php artisan elastic-create
-cd /var/www/spa && npm i && ng build && npm run gulp && npm run gulp dist
+cd /var/www/spa && npm i && npm run gulp && ng build && npm run gulp dist
