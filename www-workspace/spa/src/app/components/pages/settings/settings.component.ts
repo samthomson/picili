e0669a7e9caba45991763be01d858ff6467f8294
Sam Thomson <samt@samt.st>
2018-12-19 22:30:09 +0700

    linting plus minor bug fix

--- www-workspace/spa/src/app/components/pages/settings/settings.component.ts
+++ www-workspace/spa/src/app/components/pages/settings/settings.component.ts
@@ -1,104 +1,88 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpService } from '../../../services';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { HttpService } from "../../../services";
 
-import { GlobalVars } from './../../../../env';
+import { GlobalVars } from "./../../../../env";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html'
+  selector: "app-settings",
+  templateUrl: "./settings.component.html"
 })
 export class SettingsComponent implements OnInit {
+  private oSettings: any;
 
-    private oSettings: any;
+  private sTab: string = "file-sources";
 
-    private sTab: string = 'file-sources';
+  private sDropboxFolderPathInput: string = "";
+  private bSavingDropboxFolderPath: boolean = false;
 
-    private sDropboxFolderPathInput: string = '';
-    private bSavingDropboxFolderPath: boolean = false;
+  private sStartedSaving: string = "";
+  private sFinishedSaving: string = "";
 
-    private sStartedSaving: string = '';
-    private sFinishedSaving: string = '';
+  private bDisconnectingDropbox: boolean = false;
 
+  constructor(
+    private route: ActivatedRoute,
+    private gbl: GlobalVars,
+    private httpService: HttpService
+  ) {
+    this.oSettings = this.route.snapshot.data["userSettings"];
 
-    private bDisconnectingDropbox: boolean = false;
+    if (this.oSettings.dropbox !== null) {
+      this.sDropboxFolderPathInput = this.oSettings.dropbox.folder;
+    }
+  }
 
-    constructor(
-        private route: ActivatedRoute,
-        private gbl: GlobalVars,
-        private httpService: HttpService
+  ngOnInit() {}
+
+  goDropbox() {
+    return this.httpService.dropboxOAuth();
+  }
+
+  onSaveDropboxFolderPath() {
+    this.bSavingDropboxFolderPath = true;
+
+    this.httpService
+      .updateDropboxFolder(this.sDropboxFolderPathInput)
+      .subscribe(
+        data => {
+          this.oSettings.dropbox.sFolderPath = this.sDropboxFolderPathInput;
+
+          this.bSavingDropboxFolderPath = false;
+        },
+        err => {
+          this.bSavingDropboxFolderPath = false;
+        }
+      );
+  }
+
+  onDisconnectDropbox() {
+    // confirm
+    if (
+      confirm("Disconnect Dropbox? Picili will loose all your dropbox files.")
     ) {
-        this.oSettings = this.route.snapshot.data['userSettings'];
+      // start loading
+      this.bDisconnectingDropbox = true;
 
-        if(this.oSettings.dropbox !== null)
-        {
-            this.sDropboxFolderPathInput = this.oSettings.dropbox.folder;
-        }
+      this.httpService.disconnectDropbox().subscribe(data => {
+        // send delete to server
+        this.oSettings.dropbox = null;
+        this.bDisconnectingDropbox = false;
+      });
     }
+  }
 
-    ngOnInit() {
-    }
+  onPrivacyCheckChange(bNewValue) {
+    this.sFinishedSaving = "";
+    this.oSettings.public = bNewValue;
 
-    goDropbox()
-    {
-        return this.httpService.dropboxOAuth();
-    }
-
-    onSaveDropboxFolderPath()
-    {
-        this.bSavingDropboxFolderPath = true;
-
-        this.httpService.updateDropboxFolder(this.sDropboxFolderPathInput)
-            .subscribe(
-                (data) => {
-                    this.oSettings.dropbox.sFolderPath = this.sDropboxFolderPathInput;
-
-                    this.bSavingDropboxFolderPath = false;
-                },
-                (err) => {
-                    this.bSavingDropboxFolderPath = false;
-                }
-            );
-
-    }
-
-    onDisconnectDropbox()
-    {
-        // confirm
-        if(confirm('Disconnect Dropbox? Picili will loose all your dropbox files.'))
-        {
-            // start loading
-            this.bDisconnectingDropbox = true;
-
-            this.httpService.disconnectDropbox()
-                .subscribe(
-                (data) => {
-                    // send delete to server
-                    this.oSettings.dropbox = null;
-                    this.bDisconnectingDropbox = false;
-                }
-            );
-        }
-    }
-
-    onPrivacyCheckChange(bNewValue)
-    {
+    this.sStartedSaving = "Saving..";
+    this.httpService.updatePrivacy(bNewValue).subscribe(data => {
+      this.sStartedSaving = "";
+      this.sFinishedSaving = "Saved";
+      setTimeout(function() {
         this.sFinishedSaving = "";
-        this.oSettings.public = bNewValue;
-
-        this.sStartedSaving = "Saving..";
-        this.httpService.updatePrivacy(bNewValue)
-            .subscribe(
-                (data) => {
-                    this.sStartedSaving = "";
-                    this.sFinishedSaving = "Saved";
-                    setTimeout(
-                        function(){
-                            this.sFinishedSaving = "";
-                        },
-                        2000
-                    );
-                }
-            );
-    }
+      }, 2000);
+    });
+  }
 }
