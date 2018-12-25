eba510121c51422c06e7c6195b8839e94c9a0519
Sam Thomson <samt@samt.st>
2018-12-25 15:01:41 +0700

    ++

--- www-workspace/user-api-laravel/config/auth.php
+++ www-workspace/user-api-laravel/config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Share::class,
+            'model' => Share\User::class,
         ],
 
         // 'users' => [
