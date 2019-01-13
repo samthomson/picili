bab375d8c30ae205afb0969e63aae5c371f31356
Sam Thomson <samt@samt.st>
2019-01-13 09:53:25 +0700

    +

--- deploy-scripts/start-auto-scaler.sh
+++ deploy-scripts/start-auto-scaler.sh
@@ -1,4 +1,2 @@
 #!bin/bash
-docker-machine ssh picili
-cd /picili
-start auto-scalar `docker-compose -f docker-compose.prod.yml run -d --entrypoint="bash -c 'cd /var/www/auto-scaler && npm run forever'" workspace`
+docker-compose -f docker-compose.prod.yml run -d --entrypoint="bash -c 'cd /var/www/auto-scaler && npm run forever'" workspace`
