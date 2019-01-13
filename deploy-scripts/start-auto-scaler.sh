934fb2a7eea4af74afa7116a41e37a1899b3b700
Sam Thomson <samt@samt.st>
2019-01-13 09:54:27 +0700

    +

--- deploy-scripts/start-auto-scaler.sh
+++ deploy-scripts/start-auto-scaler.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-docker-compose -f docker-compose.prod.yml run -d --entrypoint="bash -c 'cd /var/www/auto-scaler && npm run forever'" workspace`
+docker-compose -f docker-compose.prod.yml run -d --entrypoint="bash -c 'cd /var/www/auto-scaler && npm run forever'" workspace
