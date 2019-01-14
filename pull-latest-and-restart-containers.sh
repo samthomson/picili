b691a937eb23b94b782908fb8032b08e642ab3ff
Sam Thomson <samt@samt.st>
2019-01-14 18:06:53 +0700

    better update flow

--- pull-latest-and-restart-containers.sh
+++ pull-latest-and-restart-containers.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 
-git pull
+# pull latest and disregard and local changes (spa builds)
+git fetch origin master
+git reset --hard FETCH_HEAD
+git clean -df
+# restart containers
 docker-compose -f docker-compose.prod.yml down
 docker-compose -f docker-compose.prod.yml up -d
