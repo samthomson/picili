ac76e4c9f4c9f5e7a31d765d4cee4e85977a15a4
Sam Thomson <samt@samt.st>
2019-01-14 18:04:03 +0700

    don't forget to keep workspace alive

--- deploy-scripts/elastic-reindex.sh
+++ deploy-scripts/elastic-reindex.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-docker-compose -f docker-compose.prod.yml run -d --entrypoint="bash -c 'cd /var/www/auto && php artisan elastic-delete && php artisan elastic-create && php artisan index-all'" workspace
+docker-compose run -d --entrypoint="bash -c 'cd /var/www/auto && php artisan elastic-delete && php artisan elastic-create && php artisan index-all'" workspace
