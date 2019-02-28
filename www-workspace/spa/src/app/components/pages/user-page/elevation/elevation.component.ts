3fa32203f2bf0f39c6885ae49a5d4dcf5a992d89
Sam Thomson <samt@samt.st>
2019-02-28 16:27:33 +0700

    elevation page

--- www-workspace/spa/src/app/components/pages/user-page/elevation/elevation.component.ts
+++ www-workspace/spa/src/app/components/pages/user-page/elevation/elevation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Options } from 'ng5-slider';
+
+import { HttpService, SearchService } from './../../../../services';
 
 @Component({
   selector: 'app-elevation',
@@ -6,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElevationComponent implements OnInit {
 
-  constructor() { }
+	minValue: number = 20;
+	maxValue: number = 80;
+	options: Options = {
+		floor: 0,
+		ceil: 7000,
+		step: 1
+	};
+
+	searchTimeout;
+
+
+	constructor(
+		private httpService: HttpService,
+		private searchService: SearchService
+	) { }
 
   ngOnInit() {
   }
 
+  onValueChange(bInstant: boolean = false) {
+	// trigger a search
+	// add a delay if from the slider
+	// console.log('\nmin: ', this.minValue)
+	// console.log('max: ', this.maxValue)
+
+	clearTimeout(this.searchTimeout)
+
+	// let iInterval = bInstant ? 0 : 2000
+	let iInterval = 500
+
+	this.searchTimeout = setTimeout(() => {
+		console.log('\n\nSEARCH NOW\n\n')
+
+		// build query
+		this.searchService.addElevationFilter(this.minValue, this.maxValue)
+		this.httpService.triggerSearch()
+
+	}, iInterval);
+  }
 }
