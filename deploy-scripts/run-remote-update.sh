e269d5dd2f1cf0187a5d74173688d520acdbc13d
Sam Thomson <samt@samt.st>
2021-06-26 18:17:53 +0100

    redploy to host specified in env file

--- deploy-scripts/run-remote-update.sh
+++ deploy-scripts/run-remote-update.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+source $SCRIPTPATH/../.env
+
 # bash/ssh into remote VPS and run update script there.
 # docker-machine ssh picili-app "cd /picili/deploy-scripts && bash pull-latest-and-restart.sh"
-docker-machine ssh picili "cd /picili && bash pull-latest-and-restart-containers.sh"
+docker-machine ssh $DO_HOST "cd /picili && bash pull-latest-and-restart-containers.sh"
