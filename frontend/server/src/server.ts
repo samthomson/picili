7dcc4fd6f52606e25bd288b317dd39af53f21e6d
Sam Thomson <samt@samt.st>
2021-06-30 17:17:55 +0100

    basic gql server running

---
+++ frontend/server/src/server.ts
@@ -0,0 +1,19 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+    type Query {
+        ping: String
+    }
+`
+
+const resolvers = {
+    Query: { 
+        ping: () => 'pinged'
+    },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen({ port: process.env.FRONTEND_API_PORT }).then(({ url }) => {
+	// Logger.info(`Server ready at ${url}`)
+	console.log(`server ready: url: ${url}, port: ${process.env.FRONTEND_API_PORT}`)
+})
