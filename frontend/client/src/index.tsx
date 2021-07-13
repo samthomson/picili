66337a430c2cc68676c0f59cff87cf1a3ca910d5
Sam Thomson <samt@samt.st>
2021-07-13 17:56:24 +0100

    remove webvitals

--- frontend/client/src/index.tsx
+++ frontend/client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import App from 'src/App'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 
 const uri = `${window.location.protocol}//${window.location.hostname}:3200`
 const client = new ApolloClient({
@@ -23,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
