ea373e8382505aadb16f0bee40321e8b4a3cb16a
Sam <samt@samt.st>
2021-08-03 20:31:39 +0100

    refactor api structure

--- frontend/server/src/server.ts
+++ frontend/server/src/server.ts
@@ -1,67 +1,11 @@
-import { ApolloServer, gql } from 'apollo-server-express'
 import express from 'express'
-import httpHeadersPlugin from 'apollo-server-plugin-http-headers'
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
-import * as Mutations from './mutations'
-import * as AuthUtil from './util/auth'
+import apolloServer from './api/server'
 
 const startServer = async () => {
-    const typeDefs = gql`
-        input LoginInput {
-            email: String!
-            password: String!
-        }
-        input RegisterInput {
-            email: String!
-            password: String!
-            passwordConfirmation: String!
-        }
-        type AuthResponse {
-            token: String
-            error: String
-        }
-
-        type Query {
-            ping: String
-            validateToken(token: String!): Boolean
-        }
-        type Mutation {
-            login(authInput: LoginInput!): AuthResponse
-            register(authInput: RegisterInput!): AuthResponse
-        }
-    `
-
-    const resolvers = {
-        Query: {
-            ping: (parent, args, ctx) => {
-                AuthUtil.verifyRequestIsAuthenticated(ctx)
-                return `${ctx?.userId} pinged ${Math.random()}`
-            },
-            validateToken: (parent, args, { req }) => AuthUtil.requestHasValidAuthenticationCookie(req),
-        },
-        Mutation: {
-            login: Mutations.login,
-            register: Mutations.register,
-        },
-    }
-
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        plugins: [httpHeadersPlugin, ApolloServerPluginLandingPageGraphQLPlayground],
-        context: (ctx) => {
-            return {
-                setCookies: [],
-                setHeaders: [],
-                userId: AuthUtil.userIdFromRequestCookie(ctx.req),
-            }
-        },
-    })
-
-    await server.start()
+    await apolloServer.start()
 
     const app = express()
     app.use(cookieParser())
@@ -72,7 +16,7 @@ const startServer = async () => {
     }
     app.use(cors(corsOptions))
 
-    server.applyMiddleware({
+    apolloServer.applyMiddleware({
         app,
         cors: false, // very important so that express cors middleware settings are used
     })
@@ -80,7 +24,7 @@ const startServer = async () => {
     // await new Promise((resolve) => app.listen({ port: process.env.FRONTEND_API_PORT }, resolve))
     await new Promise(() => app.listen({ port: process.env.FRONTEND_API_PORT }))
 
-    return { server, app }
+    return { apolloServer, app }
 }
 
 startServer()
