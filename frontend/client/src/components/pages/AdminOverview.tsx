bd7cdc7084fa19680557ddc095825bd406f2b85c
Sam <samt@samt.st>
2021-08-05 20:29:00 +0100

    start to fetch + display overview stats

--- frontend/client/src/components/pages/AdminOverview.tsx
+++ frontend/client/src/components/pages/AdminOverview.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react'
+import { useQuery, gql } from '@apollo/client'
 
 import AdminTemplate from 'src/components/structure/AdminTemplate'
 
+const overviewQuery = gql`
+	query overview {
+		overview {
+			unprocessedTasksCount
+		}
+	}
+`
+
 const AdminOverview: React.FunctionComponent = () => {
+	const { loading, error, data } = useQuery(overviewQuery)
+
+	if (loading) {
+		return <>loading...</>
+	}
+
+	if (error) {
+		return <>{error?.message}</>
+	}
+
 	return (
 		<AdminTemplate>
-			<div>
-				[overview stats; total files, dropbox connected or not, total
-				tags, % files with geo data, no. tasks queued]
-			</div>
+			<div>{data?.overview.unprocessedTasksCount} tasks</div>
 		</AdminTemplate>
 	)
 }
